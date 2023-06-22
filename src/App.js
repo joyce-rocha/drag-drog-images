6c68e833f0b1ad67a2aeab28114509f334024795
Joyce Rocha <joycerocha.j@gmail.com>
2023-06-22 00:22:31 -0400

    rascunho de um drag & drog images

--- src/App.js
+++ src/App.js
@@ -1,25 +1,203 @@
-import logo from './logo.svg';
-import './App.css';
+// import { upload } from "@testing-library/user-event/dist/upload";
+// import "./App.css";
+// import React, { useState, useRef } from "react";
 
-function App() {
+// function DragDrogImageUploader() {
+//   const [images, setImages] = useState([]);
+//   const [isDragging, setIsDragging] = useState(false);
+//   const fileInputRef = useRef(null);
+
+//   function selectFiles() {
+//     fileInputRef.current.click();
+//   }
+
+//   function onFileSelect(event) {
+//     const files = event.target.files;
+//     if(files.length == 0) return;
+//     for (let i = 0; i < files.length; i++) {
+//       if (files[i].type.split('/')[0] !== 'image') continue;
+//       if (!images.some((e) => e.name == file[i].name)) {
+//         setImages((prevImages) => [
+//           ...prevImages, {
+//             name: files[i].name,
+//             url: URL.createObjectURL(files[i]),
+//           },
+//         ]);
+//       }
+//     }
+//   }
+
+//   function deleteImage(index){
+//     setImages((prevImages) => prevImages.filter(( , i) => i !== index));
+//   }
+
+//   function onDragOver(event){
+//     event.preventDefault();
+//     setIsDragging(true);
+//     event.dataTransfer.dropEffect = "copy";
+//   }
+//   function onDragLeave(event){
+//     event.preventDefault();
+//     setIsDragging(false);
+//   }
+//   function onDrop(event){
+//     event.preventDefault();
+//     setIsDragging(false);
+//     const files = event.dataTransfer.files;
+//   }
+//   function uploadImages(){
+//     console.log("Images: ", images)
+
+//   }
+//   return (
+//     <div className="card">
+//       <div className="top">
+//         <p>Drag & Drop image uploading</p>
+//       </div>
+//       <div className="drag-area" onDragOver={onDragOver} onDragLeave={onDragLeave} onDrop={onDrop}>
+//       {isDragging ? (<span className="select">Drop images here</span>) : ( Drag & Drop image here or {""}
+//         <span className="select" role="button" onClick={selectFiles}>
+//           Browse
+//         </span>)}
+
+//         <input
+//           type="file"
+//           name="file"
+//           className="file"
+//           multiple
+//           ref={fileInputRef}
+//           onChange={onFileSelect}
+//         />
+//       </div>
+//       <div className="container">
+//       {
+//         images.map((images, index) => (
+//           <div className="image" key={index}>
+//           <span className="delete" onClick={() => deleteImage(index)}>&times;</span>
+//           <img src={images.url} alt={images.name} />
+//         </div>
+//         ))
+//       }
+
+//       </div>
+//       <button type="button" onClick={uploadImages}>Upload</button>
+//     </div>
+//   );
+// }
+
+// export default DragDrogImageUploader;
+
+import React, { useState, useRef } from "react";
+import "./App.css";
+
+function DragDropImageUploader() {
+  const [images, setImages] = useState([]);
+  const [isDragging, setIsDragging] = useState(false);
+  const fileInputRef = useRef(null);
+
+  function selectFiles() {
+    fileInputRef.current.click();
+  }
+
+  function onFileSelect(event) {
+    const files = event.target.files;
+    if (files.length === 0) return;
+    for (let i = 0; i < files.length; i++) {
+      if (files[i].type.split("/")[0] !== "image") continue;
+      if (!images.some((e) => e.name === files[i].name)) {
+        setImages((prevImages) => [
+          ...prevImages,
+          {
+            name: files[i].name,
+            url: URL.createObjectURL(files[i]),
+          },
+        ]);
+      }
+    }
+  }
+
+  function deleteImage(index) {
+    setImages((prevImages) => prevImages.filter((_, i) => i !== index));
+  }
+
+  function onDragOver(event) {
+    event.preventDefault();
+    setIsDragging(true);
+    event.dataTransfer.dropEffect = "copy";
+  }
+
+  function onDragLeave(event) {
+    event.preventDefault();
+    setIsDragging(false);
+  }
+
+  function onDrop(event) {
+    event.preventDefault();
+    setIsDragging(false);
+    const files = event.dataTransfer.files;
+    for (let i = 0; i < files.length; i++) {
+      if (files[i].type.split("/")[0] !== "image") continue;
+      if (!images.some((e) => e.name === files[i].name)) {
+        setImages((prevImages) => [
+          ...prevImages,
+          {
+            name: files[i].name,
+            url: URL.createObjectURL(files[i]),
+          },
+        ]);
+      }
+    }
+  }
+
+  function uploadImages() {
+    console.log("Images: ", images);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="card">
+      <div className="top">
+        <p>Drag & Drop image uploading</p>
+      </div>
+      <div
+        className="drag-area"
+        onDragOver={onDragOver}
+        onDragLeave={onDragLeave}
+        onDrop={onDrop}
+      >
+        {isDragging ? (
+          <span className="select">Drop images here</span>
+        ) : (
+          <>
+            Drag & Drop image here or{" "}
+            <span className="select" role="button" onClick={selectFiles}>
+              Browse
+            </span>
+          </>
+        )}
+        <input
+          type="file"
+          name="file"
+          className="file"
+          multiple
+          ref={fileInputRef}
+          onChange={onFileSelect}
+        />
+      </div>
+      <div className="container">
+        {images.map((image, index) => (
+          <div className="image" key={index}>
+            <span className="delete" onClick={() => deleteImage(index)}>
+              &times;
+            </span>
+            <img src={image.url} alt={image.name} />
+          </div>
+        ))}
+      </div>
+      <button type="button" onClick={uploadImages}>
+        Upload
+      </button>
     </div>
   );
 }
 
-export default App;
+export default DragDropImageUploader;
